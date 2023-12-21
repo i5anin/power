ead23d2b9436207ce93d3fbae2fe36d9666dfdb9
Isanin Sergey <sergey.isanin@gmail.com>
2023-12-22 00:18:40 +0300

    // Функция

--- tree/tree.js
+++ tree/tree.js
@@ -2,46 +2,75 @@ import fs from "fs";
 import path from "path";
 import chalk from "chalk";
 
-function listFiles(dir, prefix = "") {
-  let items;
-  try {
-    items = fs.readdirSync(dir).map((name) => ({
-      name,
-      path: path.join(dir, name),
-      isDirectory: fs.statSync(path.join(dir, name)).isDirectory()
-    }));
-  } catch (err) {
-    console.error(chalk.red(`Error reading directory ${dir}: ${err.message}`));
-    return;
-  }
+// Функция для получения символов ветвления
+function getBranchSymbols(isLast) {
+  return isLast ? "└─" : "├─";
+}
 
-  // Сортировка: сначала папки, затем файлы
-  items.sort((a, b) => {
-    if (a.isDirectory && !b.isDirectory) return -1;
-    if (!a.isDirectory && b.isDirectory) return 1;
-    return a.name.localeCompare(b.name);
-  });
+// Функция для определения цвета файла по его расширению
+function getColorByExtension(fileName) {
+  if (fileName.endsWith(".vue")) return chalk.green;
+  if (fileName.endsWith(".js")) return chalk.yellow;
+  return chalk.white;
+}
+
+// Функция для вывода содержимого директории
+function listFiles(dir, prefix = "") {
+  let items = readDirectory(dir);
+  if (!items) return;
 
   items.forEach((item, index) => {
     const isLast = index === items.length - 1;
-    try {
-      if (item.isDirectory) {
-        console.log(chalk.blue(prefix + (isLast ? "└─" : "├─") + item.name));
-        listFiles(item.path, prefix + (isLast ? "    " : "|   "));
-      } else {
-        let color = chalk.white;
-        if (item.name.endsWith(".vue")) color = chalk.green;
-        else if (item.name.endsWith(".js")) color = chalk.yellow;
+    const branchSymbol = getBranchSymbols(isLast);
 
-        console.log(color(prefix + (isLast ? "└─" : "├─") + item.name));
-      }
-    } catch (err) {
-      console.error(
-        chalk.red(`Error reading file ${item.path}: ${err.message}`)
-      );
+    if (item.isDirectory) {
+      handleDirectory(item, prefix, branchSymbol, isLast);
+    } else {
+      handleFile(item, prefix, branchSymbol);
     }
   });
 }
 
+// Функция для чтения содержимого директории
+function readDirectory(dir) {
+  try {
+    return fs
+      .readdirSync(dir)
+      .map((name) => ({
+        name,
+        path: path.join(dir, name),
+        isDirectory: fs.statSync(path.join(dir, name)).isDirectory()
+      }))
+      .sort((a, b) => sortItems(a, b));
+  } catch (err) {
+    console.error(chalk.red(`Error reading directory ${dir}: ${err.message}`));
+    return null;
+  }
+}
+
+// Функция для сортировки элементов (сначала папки, затем файлы)
+function sortItems(a, b) {
+  if (a.isDirectory && !b.isDirectory) return -1;
+  if (!a.isDirectory && b.isDirectory) return 1;
+  return a.name.localeCompare(b.name);
+}
+
+// Функция для обработки директории
+function handleDirectory(item, prefix, branchSymbol, isLast) {
+  const contents = fs.readdirSync(item.path);
+  const color = contents.length === 0 ? chalk.red : chalk.blue;
+  console.log(color(prefix + branchSymbol + item.name));
+  if (contents.length > 0) {
+    listFiles(item.path, prefix + (isLast ? "    " : "|   "));
+  }
+}
+
+// Функция для обработки файла
+function handleFile(item, prefix, branchSymbol) {
+  const color = getColorByExtension(item.name);
+  console.log(color(prefix + branchSymbol + item.name));
+}
+
+// Главная функция
 const directory = "S:/development/soft.vue.pf-forum/src"; // Используйте свой путь
 listFiles(directory);
