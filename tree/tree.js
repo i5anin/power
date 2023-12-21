218a6f36014ae073bd41e018bb7903f73a1890ca
Isanin Sergey <sergey.isanin@gmail.com>
2023-12-22 00:06:41 +0300

    chalk

--- tree/tree.js
+++ tree/tree.js
@@ -1,38 +1,31 @@
-const fs = require("fs");
-const path = require("path");
-const chalk = require("chalk");
+import fs from 'fs';
+import path from 'path';
+import chalk from 'chalk';
 
-function listFiles(dir, prefix = "") {
-  let files;
-  try {
-    files = fs.readdirSync(dir);
-  } catch (err) {
-    console.error(chalk.red(`Error reading directory ${dir}: ${err.message}`));
-    return;
-  }
-
-  files.forEach((file, index) => {
-    let fullPath = path.join(dir, file);
+function listFiles(dir, prefix = '') {
+    let files;
     try {
-      let stats = fs.statSync(fullPath);
-      if (stats.isDirectory()) {
-        console.log(
-          chalk.blue(prefix + (index === files.length - 1 ? "└─" : "├─") + file)
-        );
-        listFiles(
-          fullPath,
-          prefix + (index === files.length - 1 ? "    " : "|   ")
-        );
-      } else {
-        console.log(prefix + (index === files.length - 1 ? "└─" : "├─") + file);
-      }
+        files = fs.readdirSync(dir);
     } catch (err) {
-      console.error(
-        chalk.red(`Error reading file ${fullPath}: ${err.message}`)
-      );
+        console.error(chalk.red(`Error reading directory ${dir}: ${err.message}`));
+        return;
     }
-  });
+
+    files.forEach((file, index) => {
+        let fullPath = path.join(dir, file);
+        try {
+            let stats = fs.statSync(fullPath);
+            if (stats.isDirectory()) {
+                console.log(chalk.blue(prefix + (index === files.length - 1 ? '└─' : '├─') + file));
+                listFiles(fullPath, prefix + (index === files.length - 1 ? '    ' : '|   '));
+            } else {
+                console.log(prefix + (index === files.length - 1 ? '└─' : '├─') + file);
+            }
+        } catch (err) {
+            console.error(chalk.red(`Error reading file ${fullPath}: ${err.message}`));
+        }
+    });
 }
 
-const directory = "S:/development/soft.vue.pf-forum/src"; // Используйте свой путь
+const directory = 'S:/development/soft.vue.pf-forum/src'; // Используйте свой путь
 listFiles(directory);
