bd1ef62b57b91af00a38ae9dd6c586c815d5ab85
Isanin Sergey <sergey.isanin@gmail.com>
2023-12-22 00:02:30 +0300

    +

--- tree/tree.js
+++ tree/tree.js
@@ -1,30 +1,38 @@
 const fs = require("fs");
 const path = require("path");
+const chalk = require("chalk");
 
-function listFiles(dir, level = 0) {
+function listFiles(dir, prefix = "") {
   let files;
   try {
     files = fs.readdirSync(dir);
   } catch (err) {
-    console.error(`Error reading directory ${dir}: ${err.message}`);
+    console.error(chalk.red(`Error reading directory ${dir}: ${err.message}`));
     return;
   }
 
-  files.forEach((file) => {
+  files.forEach((file, index) => {
     let fullPath = path.join(dir, file);
     try {
       let stats = fs.statSync(fullPath);
       if (stats.isDirectory()) {
-        console.log(" ".repeat(level * 4) + "└─" + file);
-        listFiles(fullPath, level + 1);
+        console.log(
+          chalk.blue(prefix + (index === files.length - 1 ? "└─" : "├─") + file)
+        );
+        listFiles(
+          fullPath,
+          prefix + (index === files.length - 1 ? "    " : "|   ")
+        );
       } else {
-        console.log(" ".repeat(level * 4) + "└─" + file);
+        console.log(prefix + (index === files.length - 1 ? "└─" : "├─") + file);
       }
     } catch (err) {
-      console.error(`Error reading file ${fullPath}: ${err.message}`);
+      console.error(
+        chalk.red(`Error reading file ${fullPath}: ${err.message}`)
+      );
     }
   });
 }
 
-const directory = "S:\\development\\soft.vue.pf-forum\\src";
+const directory = "S:/development/soft.vue.pf-forum/src"; // Используйте свой путь
 listFiles(directory);
