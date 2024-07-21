69738be4c128d2b333ee883c1f50461e8b445da8
Isanin Sergey <sergey.isanin@gmail.com>
2024-07-21 13:06:51 +0300

    Update upgrades-for-buy-table.js

--- hamster/src/test/upgrades-for-buy-table.js
+++ hamster/src/test/upgrades-for-buy-table.js
@@ -1,68 +1,70 @@
-import https from "https";
-import { gunzipSync, inflateSync, brotliDecompressSync } from "zlib";
-import fs from "fs";
-import { headers } from "../config.js";
-import Table from "cli-table"; // Импортируем библиотеку cli-table
+import https from 'https'
+import { gunzipSync, inflateSync, brotliDecompressSync } from 'zlib'
+import fs from 'fs'
+import { headers } from '../config.js'
+import Table from 'cli-table' // Импортируем библиотеку cli-table
 
 const options = {
-  hostname: "api.hamsterkombatgame.io",
+  hostname: 'api.hamsterkombatgame.io',
   port: 443,
-  path: "/clicker/upgrades-for-buy",
-  method: "POST",
-  headers: headers,
-};
+  path: '/clicker/upgrades-for-buy',
+  method: 'POST',
+  headers: headers
+}
 
 const req = https.request(options, (res) => {
-  console.log(`statusCode: ${res.statusCode}`);
+  console.log(`statusCode: ${res.statusCode}`)
 
-  let responseData = [];
+  let responseData = []
 
-  res.on("data", (chunk) => {
-    responseData.push(chunk);
-  });
+  res.on('data', (chunk) => {
+    responseData.push(chunk)
+  })
 
-  res.on("end", () => {
+  res.on('end', () => {
     try {
-      let buffer = Buffer.concat(responseData);
-      const encoding = res.headers["content-encoding"];
+      let buffer = Buffer.concat(responseData)
+      const encoding = res.headers['content-encoding']
 
-      if (encoding === "gzip") {
-        buffer = gunzipSync(buffer);
-      } else if (encoding === "deflate") {
-        buffer = inflateSync(buffer);
-      } else if (encoding === "br") {
-        buffer = brotliDecompressSync(buffer);
+      if (encoding === 'gzip') {
+        buffer = gunzipSync(buffer)
+      } else if (encoding === 'deflate') {
+        buffer = inflateSync(buffer)
+      } else if (encoding === 'br') {
+        buffer = brotliDecompressSync(buffer)
       }
 
-      const jsonData = JSON.parse(buffer.toString());
+      const jsonData = JSON.parse(buffer.toString())
 
       if (res.statusCode === 400) {
-        console.error("Ошибка:", jsonData.message);
-        return;
+        console.error('Ошибка:', jsonData.message)
+        return
       }
 
-      // ... (код для сохранения данных в файл)
-
+      // Фильтруем апгрейды
       const availableUpgrades = jsonData.upgradesForBuy.filter(
-        (upgrade) => upgrade.cooldownSeconds === 0 && !upgrade.isExpired
-      );
+        (upgrade) =>
+          !upgrade.cooldownSeconds && // Без кулдауна
+          upgrade.isAvailable && // Доступный
+          !upgrade.isExpired // Не истёк
+      )
 
       const upgradesWithPayback = availableUpgrades.map((upgrade) => ({
         ...upgrade,
         paybackPeriod: upgrade.profitPerHour
           ? upgrade.price / upgrade.profitPerHour
-          : Infinity,
-      }));
+          : Infinity
+      }))
 
       const sortedUpgrades = upgradesWithPayback.sort(
         (a, b) => a.paybackPeriod - b.paybackPeriod
-      );
+      )
 
       // Создаем таблицу
       const table = new Table({
-        head: ["№", "Секция", "Название", "Цена", "Окупаемость", "Статус"],
-        colWidths: [4, 15, 30, 10, 15, 15],
-      });
+        head: ['№', 'Секция', 'Название', 'Цена', 'Дельта', 'Окупаемость'],
+        colWidths: [5, 10, 25, 15, 10, 15]
+      })
 
       // Добавляем данные в таблицу
       sortedUpgrades.forEach((upgrade, index) => {
@@ -70,24 +72,24 @@ const req = https.request(options, (res) => {
           index + 1,
           upgrade.section,
           upgrade.name,
-          upgrade.price,
+          upgrade.price.toLocaleString(),
+          upgrade.profitPerHourDelta.toLocaleString(),
           upgrade.paybackPeriod !== Infinity
-            ? upgrade.paybackPeriod.toFixed(2) + " ч."
-            : "бесконечность",
-          !upgrade.isAvailable ? "недоступно" : "доступно",
-        ]);
-      });
+            ? upgrade.paybackPeriod.toFixed(2) + ' ч.'
+            : 'бесконечность'
+        ])
+      })
 
       // Выводим таблицу в консоль
-      console.log(table.toString());
+      console.log(table.toString())
     } catch (error) {
-      console.error("Ошибка парсинга JSON:", error);
+      console.error('Ошибка парсинга JSON:', error)
     }
-  });
-});
+  })
+})
 
-req.on("error", (error) => {
-  console.error("Request error:", error);
-});
+req.on('error', (error) => {
+  console.error('Request error:', error)
+})
 
-req.end();
+req.end()
