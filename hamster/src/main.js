243e7fb8708c3b50c8532d1fd5613eac218c2617
Isanin Sergey <sergey.isanin@gmail.com>
2024-07-21 12:10:17 +0300

    Update main.js

--- hamster/src/main.js
+++ hamster/src/main.js
@@ -1,6 +1,115 @@
 import chalk from 'chalk'
 import { api } from './api.js'
 
+// Функция для форматирования времени
+function formatTime(seconds) {
+  const hours = Math.floor(seconds / 3600)
+  const minutes = Math.floor((seconds % 3600) / 60)
+  const remainingSeconds = seconds % 60
+  return `${hours}ч ${minutes}м ${remainingSeconds}с`
+}
+
+async function main() {
+  let secondsToBuy = Infinity // Изначально время до покупки бесконечно
+  let lastUpdateTime = 0 // Время последнего обновления информации
+
+  while (true) {
+    try {
+      // Обновляем информацию каждые 5 минут или если время до покупки не определено
+      if (
+        Date.now() - lastUpdateTime >= 5 * 60 * 1000 ||
+        secondsToBuy === Infinity
+      ) {
+        // Получаем актуальный баланс и информацию о пассивном доходе
+        const clickerUser = await getBalance()
+        if (!clickerUser) continue
+
+        const {
+          balanceCoins: balance,
+          earnPassivePerSec,
+          earnPassivePerHour
+        } = clickerUser
+
+        // Выводим информацию о текущем состоянии счета
+        console.log(
+          `[${new Date().toLocaleTimeString()}] ` +
+            `• Баланс: ` +
+            `${chalk.yellow(Math.round(balance).toLocaleString())} ` +
+            `• Прирост: ` +
+            `${chalk.yellow(earnPassivePerHour.toLocaleString())} в час ` +
+            `${chalk.yellow(earnPassivePerSec.toLocaleString())} в сек `
+        )
+
+        // Получаем список доступных апгрейдов
+        const data = await api.getUpgradesForBuy()
+        const availableUpgrades = data.upgradesForBuy
+          .filter(
+            (upgrade) =>
+              !upgrade.cooldownSeconds &&
+              upgrade.isAvailable &&
+              !upgrade.isExpired
+          )
+          .map((upgrade) => ({
+            ...upgrade,
+            paybackPeriod: upgrade.profitPerHour
+              ? upgrade.price / upgrade.profitPerHour
+              : Infinity
+          }))
+          .sort((a, b) => a.paybackPeriod - b.paybackPeriod)
+
+        // Находим ближайший доступный апгрейд
+        const nearestUpgrade = availableUpgrades.find(
+          (upgrade) => upgrade.price > balance
+        )
+
+        // Если есть ближайший апгрейд, вычисляем время до его покупки
+        if (nearestUpgrade) {
+          const priceDifference = nearestUpgrade.price - balance
+          secondsToBuy = Math.ceil(priceDifference / earnPassivePerSec)
+
+          console.log(
+            chalk.blue(
+              `Ближайший апгрейд: ${nearestUpgrade.name} (${nearestUpgrade.price.toLocaleString()})`
+            )
+          )
+        } else {
+          secondsToBuy = Infinity
+          console.log(chalk.blue('Нет доступных апгрейдов для покупки.'))
+        }
+
+        lastUpdateTime = Date.now()
+      }
+
+      // Выводим время до покупки
+      console.log(chalk.blue(`Время до покупки: ${formatTime(secondsToBuy)}`))
+
+      // Проверяем, пришло ли время покупать, каждую секунду
+      if (secondsToBuy <= 0) {
+        console.log(chalk.green('Пришло время покупать!'))
+
+        // Получаем ID ближайшего апгрейда (он мог измениться)
+        const data = await api.getUpgradesForBuy()
+        const nearestUpgrade = data.upgradesForBuy.find(
+          (upgrade) => upgrade.price > balance
+        )
+
+        if (nearestUpgrade) {
+          await api.buyUpgrade(nearestUpgrade.id)
+          secondsToBuy = Infinity // Сбрасываем время до покупки
+          lastUpdateTime = 0 // Обновляем время последнего обновления, чтобы получить данные о новом апгрейде
+        }
+      } else {
+        secondsToBuy-- // Уменьшаем время до покупки каждую секунду
+      }
+    } catch (error) {
+      console.error('Ошибка в главном цикле:', error)
+    }
+
+    // Ждём 1 секунду
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+  }
+}
+
 // Получаем актуальный баланс и информацию о пассивном доходе
 async function getBalance() {
   try {
@@ -9,113 +118,6 @@ async function getBalance() {
   } catch (error) {
     console.error('Ошибка при обновлении баланса:', error)
     return null
-  }
-}
-
-async function main() {
-  while (true) {
-    try {
-      // Получаем актуальный баланс и информацию о пассивном доходе
-      const clickerUser = await getBalance()
-      if (!clickerUser) continue // Если произошла ошибка при получении данных, пропускаем итерацию
-
-      const {
-        balanceCoins: balance,
-        earnPassivePerSec,
-        earnPassivePerHour
-      } = clickerUser
-
-      console.log(
-        `[${new Date().toLocaleTimeString()}] ` +
-          `• Баланс: ` +
-          `${chalk.yellow(Math.round(balance).toLocaleString())} ` +
-          `• Прирост: ` +
-          `${chalk.yellow(earnPassivePerHour.toLocaleString())} в час ` +
-          `${chalk.yellow(earnPassivePerSec.toLocaleString())} в сек `
-      )
-
-      // Получаем список доступных апгрейдов
-      const data = await api.getUpgradesForBuy()
-      const availableUpgrades = data.upgradesForBuy
-        .filter(
-          (upgrade) =>
-            !upgrade.cooldownSeconds &&
-            upgrade.isAvailable &&
-            !upgrade.isExpired
-        )
-        .map((upgrade) => ({
-          ...upgrade,
-          paybackPeriod: upgrade.profitPerHour
-            ? upgrade.price / upgrade.profitPerHour
-            : Infinity
-        }))
-        .sort((a, b) => a.paybackPeriod - b.paybackPeriod)
-        .slice(0, 10)
-
-      // Фильтруем апгрейды по цене
-      const affordableUpgrades = availableUpgrades.filter(
-        (upgrade) => upgrade.price <= balance
-      )
-
-      // Если есть доступные апгрейды, покупаем лучший
-      if (affordableUpgrades.length > 0) {
-        const bestUpgrade = affordableUpgrades[0]
-        const upgradeIndex = availableUpgrades.indexOf(bestUpgrade) + 1
-        console.log(
-          `Покупаю ` +
-            `(место ${upgradeIndex} в топ-10):` +
-            `${bestUpgrade.section}: ${bestUpgrade.name} ${bestUpgrade.price}` +
-            `- окупаемость: ${bestUpgrade.paybackPeriod.toFixed(2)} ч.`
-        )
-        await api.buyUpgrade(bestUpgrade.id)
-      } else {
-        console.log(
-          chalk.red('Нет доступных для покупки апгрейдов, подходящих по цене')
-        )
-
-        // Находим ближайший доступный апгрейд
-        const nearestUpgrade = availableUpgrades
-          .filter((upgrade) => upgrade.price > balance)
-          .sort((a, b) => a.price - b.price)[0]
-
-        // Выводим информацию о ближайшем апгрейде
-        if (nearestUpgrade) {
-          const priceDifference = nearestUpgrade.price - balance
-          const secondsToBuy = Math.ceil(priceDifference / earnPassivePerSec)
-          const hoursToBuy = Math.floor(secondsToBuy / 3600)
-          const minutesToBuy = Math.floor((secondsToBuy % 3600) / 60)
-          const secondsLeft = secondsToBuy % 60
-
-          console.log(
-            chalk.blue(
-              `Ближайший доступный апгрейд: ${nearestUpgrade.section}: ` +
-                `${nearestUpgrade.name} ${nearestUpgrade.price.toLocaleString()} ` +
-                `- окупаемость: ${
-                  nearestUpgrade.paybackPeriod !== Infinity
-                    ? nearestUpgrade.paybackPeriod.toFixed(2) + ' ч.'
-                    : 'бесконечность'
-                }` +
-                `\n Время до покупки: ${hoursToBuy}ч ${minutesToBuy}м ${secondsLeft}с`
-            )
-          )
-
-          // Проверяем, пришло ли время покупать
-          if (hoursToBuy === 0 && minutesToBuy === 0 && secondsLeft <= 6) {
-            console.log(chalk.green('Пришло время покупать!'))
-            await api.buyUpgrade(nearestUpgrade.id)
-            // После покупки сбрасываем availableUpgrades, чтобы не пытаться купить тот же апгрейд снова
-            availableUpgrades.length = 0
-          }
-        } else {
-          console.log(chalk.blue('Нет апгрейдов для показа.'))
-        }
-      }
-    } catch (error) {
-      console.error('Ошибка в главном цикле:', error)
-    }
-
-    // Ждём 6 секунд
-    await new Promise((resolve) => setTimeout(resolve, 6 * 1000))
   }
 }
 
