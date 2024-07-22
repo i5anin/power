5efd7f3e7429d2fa76888babc034a488c2716298
Isanin Sergey <sergey.isanin@gmail.com>
2024-07-22 07:41:12 +0300

    Update config.js

--- hamster/src/test/config.js
+++ hamster/src/test/config.js
@@ -1,26 +1,26 @@
 export const headers = {
-  "Content-Type": "application/json",
+  'Content-Type': 'application/json',
   Authorization:
-    "Bearer 1721286018267pTT5u2xltkvWbpvEjmFYIxFK3cD3RyOEkr05QjSRzmbLucVnu5PcWU9PsgUJCnT5390895078",
-  Accept: "application/json",
-  "Accept-Encoding": "gzip, deflate, br",
-  "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-  "Cache-Control": "no-cache",
-  Dnt: "1",
-  Origin: "https://hamsterkombatgame.io",
-  Pragma: "no-cache",
-  Priority: "u=1, i",
-  Referer: "https://hamsterkombatgame.io/",
-  "Sec-Ch-Ua":
+    'Bearer 17214811846816Hd8GWDg5B7XlEvIctB0ug6fZ8FM1MkptdjB8u3Q419jD76RwhUuGppYH6sYecfc5049295393',
+  Accept: 'application/json',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+  'Cache-Control': 'no-cache',
+  Dnt: '1',
+  Origin: 'https://hamsterkombatgame.io',
+  Pragma: 'no-cache',
+  Priority: 'u=1, i',
+  Referer: 'https://hamsterkombatgame.io/',
+  'Sec-Ch-Ua':
     '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-  "Sec-Ch-Ua-Mobile": "?0",
-  "Sec-Ch-Ua-Platform": '"Windows"',
-  "Sec-Fetch-Dest": "empty",
-  "Sec-Fetch-Mode": "cors",
-  "Sec-Fetch-Site": "same-site",
-  "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
-};
+  'Sec-Ch-Ua-Mobile': '?0',
+  'Sec-Ch-Ua-Platform': '"Windows"',
+  'Sec-Fetch-Dest': 'empty',
+  'Sec-Fetch-Mode': 'cors',
+  'Sec-Fetch-Site': 'same-site',
+  'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+}
 
 // Bearer 1721286018267pTT5u2xltkvWbpvEjmFYIxFK3cD3RyOEkr05QjSRzmbLucVnu5PcWU9PsgUJCnT5390895078
 
